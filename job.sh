61cbc521e9118ada2fa9fe257a5b1d70b9749269
walidgeuttala <walidgeuttala@gmail.com>
2024-10-20 00:22:25 +0200

    save

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -A p_gnn001               # Account name to be debited
 #SBATCH --job-name=tsp22          # Job name
-#SBATCH --time=0-06:00:00        # Maximum walltime (30 minutes)
-#SBATCH --partition=gpu           # Select the ai partition
-#SBATCH --gres=gpu:1          # Request 1 to 4 GPUs per node
-#SBATCH --mem-per-cpu=30000       # Memory per CPU core (16 GB)
+#SBATCH --time=0-10:00:00        # Maximum walltime (30 minutes)
+#SBATCH --partition=cpu           # Select the ai partition
+## --gres=gpu:1          # Request 1 to 4 GPUs per node
+#SBATCH --mem-per-cpu=100000       # Memory per CPU core (16 GB)
 #SBATCH --nodes=1               # Request 1 node
 
 # Your job commands here
